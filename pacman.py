6604c631634eb8e230a0e61125ed115bfd4eb689
rossiedu <eduardorossi30@gmail.com>
2022-06-19 12:22:53 -0300

    primeiro desenho

--- pacman.py
+++ pacman.py
@@ -1,4 +1,25 @@
+# Importando e inicializando a bilbioteca
 import pygame
-
 pygame.init()
 
+yellow = (255,255,0)
+# Definindo o tamanho da janela do jogo (altura, largura)
+tela = pygame.display.set_mode((640,480),0)
+
+# Criando um looping para que a janela fique aberta até ser fechada
+while True:
+    # Regras
+    # Cores
+    pygame.draw.circle(
+        surface = tela,
+        color = yellow,
+        center = (320,240),
+        radius = 50,
+        width = 0
+    )
+    pygame.display.update()
+    # Eventos
+    ## Sair do loop quando o usuario clicar no fechar a janela
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            exit()
