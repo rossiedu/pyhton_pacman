80cde3de90960916fd5a5f98d38317331ce92c44
rossiedu <eduardorossi30@gmail.com>
2022-06-19 15:40:03 -0300

    classes create_screen e pacman criadas

--- pacman.py
+++ pacman.py
@@ -1,54 +1,72 @@
-# Importando e inicializando a bilbioteca
+# Preparando o ambiente
 import pygame
 pygame.init()
 
+class create_screen():
+    def __init__(self,width,height,rgb_color):
+        self.width = width
+        self.height = height
+        self.RGB = rgb_color
+    
+    def create_surface(self):
+        screen = pygame.display.set_mode(
+            (self.width,self.height),
+            0
+        )
+        screen.fill(self.RGB)
+        pygame.display.flip()
+        return screen
 
-# Declarando as variaveis
-## Variaveis de tela
-height = 480
-width = 640
-## Variaveis do pacman
-yellow = (255,255,0)
-black = (0,0,0)
-radius = 30
-x = 10
-y = 10
-velocity = 0.3
-velocity_x = velocity
-velocity_y = velocity
+    def pacman_center_x(self):
+        x_center = self.width / 2
+        return x_center
 
-## Definindo o tamanho da janela do jogo (largura, altura)
-tela = pygame.display.set_mode((width,height),0)
+    def pacman_center_y(self):
+        y_center = self.height / 2
+        return y_center
 
-# Criando um looping para que a janela fique aberta até ser fechada
-while True:
-    # Regras
-    x += velocity_x
-    y += velocity_y
+    def background_color(self):
+        color = self.RGB
+        return color
 
-    if x + radius > width:
-        velocity_x = -velocity
-    if x - radius < 0:
-        velocity_x = velocity
-    if y + radius > height:
-        velocity_y = -velocity
-    if y - radius  < 0:
-        velocity_y = velocity
 
-    # Cores
-    ## Pintando a tela de preto
-    tela.fill(black)
-    ## Desenhando o pacman
-    pygame.draw.circle(
-        surface = tela,
-        color = yellow,
-        center = (int(x),int(y)),
-        radius = radius,
-        width = 0
-    )
-    pygame.display.update()
-    # Eventos
-    ## Sair do loop quando o usuario clicar no fechar a janela
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            exit()
+class pacman():
+    def __init__(self,tamanho,screen,surface,color):
+        self.tamanho = tamanho
+        self.x_center = screen.pacman_center_x()
+        self.y_center = screen.pacman_center_y()
+        self.background_color = screen.background_color()
+        self.screen = screen
+        self.surface = surface
+        self.color = color
+        self.radius = tamanho / 2
+
+    def draw_circle(self):
+        surface = self.surface
+        color = self.color
+        radius = self.radius
+        x_center = self.x_center
+        y_center = self.y_center
+        circle = pygame.draw.circle(surface,color,(x_center,y_center),radius,0)
+        return circle
+    
+    def draw_polygon(self):
+       color_black = (0,0,0)
+       point_one = (self.x_center,self.y_center)
+       point_two = (self.x_center + self.radius, self.y_center - self.radius)
+       point_three = (self.x_center + self.radius, self.y_center)
+       surface = self.surface
+       dots = [point_one,point_two,point_three]
+       polygon = pygame.draw.polygon(surface,self.background_color,dots,0)
+       return polygon
+    
+    def draw_small_circle(self):
+        surface = self.surface
+        color = (0,0,0)
+        radius = int(self.radius / 10)
+        x_center = int(self.x_center + (self.radius / 2))
+        y_center = int(self.y_center - (self.radius / 2))
+        small_circle = pygame.draw.circle(surface,color,(x_center,y_center),radius,0)
+        return small_circle
+
+    
